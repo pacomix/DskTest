e195e0dc20abc0a4c4306d7fffe8e9942dcc1325
Pacomix <pacomix@hotmail.com>
2026-01-26 20:37:18 +0100

    [CHANGE] Deleted unused file and re-added omitted typedefs.

--- src/main.h
+++ src/main.h
@@ -1,4 +1,9 @@
-#include "types.h"
+typedef unsigned char U8;
+typedef unsigned short int U16;
+typedef signed char S8;
+typedef signed short int S16;
+typedef unsigned long int U32;
+typedef signed long int I32;
 
 #define false 0
 #define true 1
