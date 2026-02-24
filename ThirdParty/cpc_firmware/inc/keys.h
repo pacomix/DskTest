cd82b73a2ed748f925c112228d28103073571e7d
Pacomix <pacomix@hotmail.com>
2026-02-25 00:41:23 +0100

    [CHANGE] Refactore functionality and GUI leaving it minimalistic.

--- ThirdParty/cpc_firmware/inc/keys.h
+++ ThirdParty/cpc_firmware/inc/keys.h
@@ -3,14 +3,21 @@
 
 #include "firmware.h"
 
-#define CHAR_ENTER_BIG 0x0D
-#define CHAR_ENTER_SMALL 0x8B
-#define CHAR_COPY 0xE0
+// NOTE - Firmware functions uses ASCII codes for common keys
+// but special ones for others like Fx keys, cursor keys, etc...
+// So it is safe to use something like the below to check for keypresses:
+// 
+// if ('\n' == firm_read_key())
+//
+#define FIRM_KEY_ENTER_BIG 0x0D
 
-#define CHAR_CURSOR_UP 0xF0
-#define CHAR_CURSOR_DOWN 0xF1
-#define CHAR_CURSOR_LEFT 0xF2
-#define CHAR_CURSOR_RIGHT 0xF3
+// Amstrad specific
+#define FIRM_KEY_ENTER_SMALL 0x8B
+#define FIRM_KEY_COPY 0xE0
+#define FIRM_KEY_CURSOR_UP 0xF0
+#define FIRM_KEY_CURSOR_DOWN 0xF1
+#define FIRM_KEY_CURSOR_LEFT 0xF2
+#define FIRM_KEY_CURSOR_RIGHT 0xF3
 
 U8 firm_get_key_wait(void) __sdcccall(1);
 U8 firm_read_key    (void) __sdcccall(1);
--- src/main.c
+++ src/main.c
@@ -11,60 +11,44 @@
 
 // Variables defined in crt0_cpc.s to avoid static initialization code.
 // TODO - This could also be defined inside a __naked function within an __asm __endasm; block for better clarity...
-extern U8 uKeyPressed;
+extern U8 g_u8KeyPressed;
 extern U8 g_szBytes[6];  // Temp buffer used to convert from integer/byte to ascii
-extern U16 g_sTime;  // Time variable. Contains amount of interruptions happened.
+extern U16 g_u16Time;  // Time variable. Contains amount of interruptions happened.
 extern U8 g_realTime[5];
 extern U8 g_realConstant18000[5];
 extern U8 g_realHalf[5];
 
 extern bool g_bSectorIDNotFound;
-extern U16 uRPMs;
-extern U8 uRPMsDec;
-extern U16 uLoops;
-extern U8 uPartialSecs;
-extern U16 uPartialInts;
-extern U8 uReadSectors;
-extern U16 uReadBytes;
+extern U16 g_u16RPMs;
+extern U8 g_u8RPMsDec;
+extern U16 g_u16Loops;
+extern U8 g_u8PartialSecs;
+extern U16 g_u16PartialInts;
 extern U8 g_realLoops[5];
 
-#define OPTION_COUNT 9
+#define OPTION_COUNT 3
 extern const U8 szOptions;
 
 extern const U8 szInfoMsg;
-extern U8 uSelectedOption;
+extern U8 g_u8SelectedOption;
 
-#define OPT_DRIVE 0
-#define OPT_MOTOR 1
-#define OPT_TRACK 2
-#define OPT_SECTI 3
-#define OPT_RPM   4
-#define OPT_UPD   5
-#define OPT_READ_SECTORS  6
-#define OPT_READ_BYTES  7
-#define OPT_TEST_TRACKS  8
+#define OPT_TEST_RPM   0
+#define OPT_TEST_SECID 1
+#define OPT_TEST_TRACKS  2
 
-#define POS_Y_STAT_DRIVE 1
-#define POS_Y_STAT_MOTOR 2
-#define POS_Y_STAT_TRACK 3
-#define POS_Y_STAT_SECTI 4
-#define POS_Y_STAT_RPM   5
-#define POS_Y_STAT_UPD   6
-#define POS_Y_STAT_READ_SECTORS  7
-#define POS_Y_STAT_READ_BYTES  8
-#define POS_Y_STAT_TEST_TRACKS  9
+#define POS_Y_TEST_RPM   3
+#define POS_Y_FIND_SECID 4
+#define POS_Y_STAT_TEST_TRACKS  5
 
-#define POS_X_STAT_DRIVE 21
+#define POS_Y_STATS 1
+
+// Main stat bar
+#define POS_X_STAT_DRIVE 9
 #define POS_X_STAT_MOTOR 21
-#define POS_X_STAT_TRACK 21
-#define POS_X_STAT_SECTI 21
-#define POS_X_STAT_SECTI2 36
-#define POS_X_STAT_RPM   5
-#define POS_X_STAT_UPD   21
-#define POS_X_STAT_READ_SECTORS 21
-#define POS_X_STAT_READ_BYTES   21
-#define POS_X_STAT_TEST_TRACKS_TRACK_ID   21
-#define POS_X_STAT_TEST_TRACKS_SECTOR_ID   27
+#define POS_X_STAT_UPD   37
+#define POS_X_STAT_TRACK 50
+#define POS_X_STAT_SECID 66
+#define POS_X_STAT_SECID_FOUND 77
 
 
 // Our interruption function. It simply increments our timer.
@@ -75,9 +59,9 @@ void myIntFunc(void) __naked {
     di
     push af
     push hl
-    ld hl, (#_g_sTime)
+    ld hl, (#_g_u16Time)
     inc hl
-    ld (#_g_sTime), hl
+    ld (#_g_u16Time), hl
     pop hl
     pop af
     ei
@@ -92,11 +76,11 @@ void enable_my_int(void) __naked __sdcccall(1) {
 __asm
   di
 
-	ld   hl,           (#ADDR_FIRMWARE_JP_ADDR)
-	ld   (#_old_int),  hl
+	ld hl, (#ADDR_FIRMWARE_JP_ADDR)
+	ld (#_old_int),  hl
 
-	ld   hl,           #_myIntFunc
-	ld   (#ADDR_FIRMWARE_JP_ADDR),    hl
+	ld hl, #_myIntFunc
+	ld (#ADDR_FIRMWARE_JP_ADDR),    hl
 
 	ei
 	ret
@@ -107,8 +91,8 @@ void disable_my_int(void) __naked __sdcccall(1) {
 __asm
   di
 
-	ld   hl,           (#_old_int)
-	ld   (#ADDR_FIRMWARE_JP_ADDR),  hl
+	ld hl, (#_old_int)
+	ld (#ADDR_FIRMWARE_JP_ADDR),  hl
 
 	ei
 	ret
@@ -131,64 +115,57 @@ void printNum(U16 uNum, U16 uBase) {
     uNum -= uBase * uTemp;
     uBase /= 10;
   } while(uBase != 0);
-  g_szBytes[5] = '\0';
+  g_szBytes[uStrIdx] = '\0';
 
   printText(g_szBytes);
 }
 
+void printByte(U8 u8Num, U16 u16Base) {
+  printNum((U16) u8Num, u16Base);
+}
 
-void printInt(U16 uByte) {
-  printNum(uByte, 10000);
+void printInt(U16 u16Num, U16 u16Base) {
+  printNum(u16Num, u16Base);
 }
 
 
 static void printStatusDrives(void) {
   // Current selected drive
-  firm_set_cursor_at(POS_X_STAT_DRIVE, POS_Y_STAT_DRIVE);
+  firm_set_cursor_at(POS_X_STAT_DRIVE, POS_Y_STATS);
   firm_put_char(65 + g_fdc_u8HeadDriveSelection);
 }
 
 static void printStatusMotor(void) {
-  printText(g_fdc_u8MotorOn ? "\x1F\x15\x2ON!" : "\x1F\x15\x2OFF");
+  printText(g_fdc_u8MotorOn ? "\x1F\x15\x1ON!" : "\x1F\x15\x1OFF");
 }
 
 static void printStatusTrack(void) {
   // Current selected track
-  firm_set_cursor_at(POS_X_STAT_TRACK, POS_Y_STAT_TRACK);
-  printInt((U16)g_fdc_u8TrackSelection);
+  firm_set_cursor_at(POS_X_STAT_TRACK, POS_Y_STATS);
+  printByte(g_fdc_u8TrackSelection, 100);
 }
 
 static void printStatusSectorID(void) {
   // Current selected Sector ID and result
-  firm_set_cursor_at(POS_X_STAT_SECTI, POS_Y_STAT_SECTI);
-  printInt((U16)g_fdc_u8SectorSelection);
-  printText(g_bSectorIDNotFound ? "\x1F\x24\x04NO!" : "\x1F\x24\x04YES");
+  firm_set_cursor_at(POS_X_STAT_SECID, POS_Y_STATS);
+  printByte(g_fdc_u8SectorSelection, 100);
+  printText(g_bSectorIDNotFound ? "\x1F\x4D\x01NO!" : "\x1F\x4D\x01YES");
 }
 
 static void printStatusUpdSecs(void) {
-  firm_set_cursor_at(POS_X_STAT_UPD, POS_Y_STAT_UPD);
-  printInt((U16) uPartialSecs);
-}
-
-static void printStatusReadSectors(void) {
-  firm_set_cursor_at(POS_X_STAT_READ_SECTORS, POS_Y_STAT_READ_SECTORS);
-  printInt((U16) uReadSectors);
-}
-
-static void printStatusReadBytes(void) {
-  firm_set_cursor_at(POS_X_STAT_READ_BYTES, POS_Y_STAT_READ_BYTES);
-  printInt(uReadBytes);
+  firm_set_cursor_at(POS_X_STAT_UPD, POS_Y_STATS);
+  printByte(g_u8PartialSecs, 100);
 }
 
 void printStatusRPMs(void) {
   /*
   float fRPMs;
-  fRPMs = (uLoops * 36000.0f) / (g_sTime * 1.0f);
-  uRPMs = fRPMs; // integer part of the division
-  uRPMsDec = (fRPMs - uRPMs) * 100.0f;
+  fRPMs = (g_u16Loops * 36000.0f) / (g_u16Time * 1.0f);
+  g_u16RPMs = fRPMs; // integer part of the division
+  g_u8RPMsDec = (fRPMs - g_u16RPMs) * 100.0f;
   */
 
-  ////fRPMs = ((uLoops * 300) * 60.0f) / (g_sTime * 1.0f);
+  ////fRPMs = ((g_u16Loops * 300) * 60.0f) / (g_u16Time * 1.0f);
   // Calc the 0.5f and the 300 * 60  real
   firm_integer_to_real(1, g_realHalf);
   firm_integer_to_real(2, g_realLoops);
@@ -196,35 +173,29 @@ void printStatusRPMs(void) {
 
   firm_integer_to_real(36000, g_realConstant18000);
 
-  firm_integer_to_real(g_sTime, g_realTime);
-  firm_integer_to_real(uLoops, g_realLoops);
+  firm_integer_to_real(g_u16Time, g_realTime);
+  firm_integer_to_real(g_u16Loops, g_realLoops);
 
   firm_real_multiplication(g_realLoops, g_realConstant18000);
   firm_real_division(g_realLoops, g_realTime);
 
   firm_real_sub(g_realHalf, g_realLoops);  // subtract 0.5 since real_to_integer rounds up when decs. >= 0.5 or down when decs. < 0.5
-  uRPMs = firm_real_to_integer(g_realHalf);
+  g_u16RPMs = firm_real_to_integer(g_realHalf);
 
-  firm_integer_to_real((U16)uRPMs, g_realTime);
+  firm_integer_to_real(g_u16RPMs, g_realTime);
   firm_real_sub(g_realTime, g_realLoops);
   firm_integer_to_real((U16)100, g_realLoops);
   firm_real_multiplication(g_realTime, g_realLoops);
-  uRPMsDec = (U8) firm_real_to_integer(g_realTime);
+  g_u8RPMsDec = (U8) firm_real_to_integer(g_realTime);
 
   // Print the current calculated RPMs
-  firm_set_cursor_at(24, POS_Y_STAT_RPM);
-  printInt((U16)uRPMsDec);
-  printText("\x1F\x1A\x05.");
-  firm_set_cursor_at(21, POS_Y_STAT_RPM);
-  printInt(uRPMs);
+  firm_set_cursor_at(23, POS_Y_TEST_RPM);
+  printByte(g_u8RPMsDec, 10);
+  printText("\x1F\x16\x03.");
+  firm_set_cursor_at(19, POS_Y_TEST_RPM);
+  printInt(g_u16RPMs, 100);
   
 }
-
-TODO - reestructurar opciones
-motor on/off - asignar una tecla
-drive - asignar tecla
-
-read bytes - enter y meter numero
 
 
 void printLabels(void) {
@@ -233,20 +204,23 @@ void printLabels(void) {
   printStatusTrack();
   printStatusSectorID();
   printStatusUpdSecs();
-  printStatusReadSectors();
-  printStatusReadBytes();
   
-  // printLabel(&szOptions, 1);
   printText(&szOptions);
-  firm_set_cursor_at(1, uSelectedOption+1);  printText(">");
+  firm_set_cursor_at(1, g_u8SelectedOption+3);
+  printText(">");
 }
 
+static void prepareHeadToTrack(void) {
+  fdc_TurnMotorOn();
+  fdc_Recalibrate();
+  fdc_Recalibrate();
+  fdc_GoToTrack();
+}
 
 static void startRPMs(void) {
   U8 counter;
-  printText("\x1F\x0A\x05STARTING");
-  fdc_TurnMotorOn();
-  fdc_GoToTrack();
+  
+  prepareHeadToTrack();
 
   do { // Look for a missing address mark error track and sector
     counter = 0;
@@ -257,17 +231,14 @@ static void startRPMs(void) {
     printStatusSectorID();
   } while(!g_bSectorIDNotFound);
 
-  // Start measuring
-  printText("\x1F\x0A\x05RUNNING!");
-
-  uLoops = 0;
-  g_sTime = 1;  // Start measuring
+  g_u16Loops = 0;
+  g_u16Time = 1;  // Start measuring
 }
 
 static void measureRPMs(void) {
-  if (g_sTime) {
+  if (g_u16Time) {
     // FindSector with a wrong sector ID will finish after 2 full rotations
-    // of the disc, so uLoops will end up having the number of rotations / 2.
+    // of the disc, so g_u16Loops will end up having the number of rotations / 2.
     // Start with syncing the hole...
     fdc_FindSector();
 
@@ -275,41 +246,28 @@ static void measureRPMs(void) {
     enable_my_int();
     fdc_FindSector();    
     disable_my_int();
-    uLoops++;
+    g_u16Loops++;
 
-    // Print stats every uPartialSecs
-    if (g_sTime > (uPartialSecs * 150)) {
-      g_sTime--;
+    // Print stats every g_u8PartialSecs
+    if (g_u16Time > (g_u8PartialSecs * 150)) {
+      g_u16Time--;
       printStatusRPMs();
-      g_sTime = 1;
-      uLoops = 0;
+      g_u16Time = 1;
+      g_u16Loops = 0;
     }
   }
 }
 
-void myReadBytes(void) {
-
-  U8 uTrack = g_fdc_u8TrackSelection;
-  U8 uSector = g_fdc_u8SectorSelection;
-  U8* pTargetAddr = (U8*) 0xC000;
-
-  fdc_GoToTrack();
-  fdc_read_bytes(pTargetAddr, uReadBytes);
-
-  g_fdc_u8TrackSelection = uTrack;
-  g_fdc_u8SectorSelection = uSector;
-}
-
 void testTracks(void) {
+  U8 u8TargetTrack = g_fdc_u8TrackSelection+1;
+  U8 u8TargetSector = g_fdc_u8SectorSelection + 9;
+  U8 u8InitialSector = g_fdc_u8SectorSelection;
 
-  g_fdc_u8TrackSelection = 41;
-
-  fdc_TurnMotorOn();
-  fdc_GoToTrack();
-  for (g_fdc_u8TrackSelection = 0; g_fdc_u8TrackSelection < 42; g_fdc_u8TrackSelection++) {
+  prepareHeadToTrack();
+  for (g_fdc_u8TrackSelection = 0; g_fdc_u8TrackSelection < u8TargetTrack; g_fdc_u8TrackSelection++) {
     fdc_GoToTrack();
     printStatusTrack();
-    for (g_fdc_u8SectorSelection = 193; g_fdc_u8SectorSelection < 202; g_fdc_u8SectorSelection++) {
+    for (g_fdc_u8SectorSelection = u8InitialSector; g_fdc_u8SectorSelection < u8TargetSector; g_fdc_u8SectorSelection++) {
       do {
         g_bSectorIDNotFound = fdc_FindSector();
         printStatusSectorID();
@@ -317,104 +275,67 @@ void testTracks(void) {
     }
   }
   fdc_TurnMotorOff();
+  g_fdc_u8SectorSelection = u8InitialSector;
 }
 
 void main(void) {
 
   fdc_toggleDrives();
   
-  firm_set_palette_color(0, (U16) 0x1B1B);
-  firm_set_palette_color(1, (U16) 0);
+  firm_set_palette_color(0, (U16) 0);
+  firm_set_palette_color(1, (U16) 0x1B1B);
 
   printText(&szInfoMsg);
   printLabels();
   
-  do {  // app loop
-    uKeyPressed = firm_read_key();
-    if (uKeyPressed) {
-
-      if (uKeyPressed == CHAR_CURSOR_UP && uSelectedOption != 0) {
-        uSelectedOption -= 1;
-
-      } else if (uKeyPressed == CHAR_CURSOR_DOWN && uSelectedOption < (OPTION_COUNT-1)) {
-        uSelectedOption += 1;
-
-      } else if (uKeyPressed == CHAR_CURSOR_LEFT) {
-        if (OPT_TRACK == uSelectedOption) {
-          g_fdc_u8TrackSelection--;
-
-        } else if (OPT_SECTI == uSelectedOption) {
-          g_fdc_u8SectorSelection--;
-
-        } else if (OPT_UPD == uSelectedOption) {
-          uPartialSecs -= 1;
-          uPartialInts -= 300;
-
-        } else if (OPT_READ_SECTORS == uSelectedOption) {
-          uReadSectors--;
-          uReadBytes = uReadSectors << 9;
-
-        } else if (OPT_READ_BYTES == uSelectedOption) {
-          uReadBytes--;
-
-        }
-
-      } else if (uKeyPressed == CHAR_CURSOR_RIGHT) {
-        if (OPT_TRACK == uSelectedOption) {
-          g_fdc_u8TrackSelection++;
-
-        } else if (OPT_SECTI == uSelectedOption) {
-          g_fdc_u8SectorSelection++;
-
-        } else if (OPT_UPD == uSelectedOption) {
-          uPartialSecs += 1;
-          uPartialInts += 300;
-
-        } else if (OPT_READ_SECTORS == uSelectedOption) {
-          uReadSectors++;
-          uReadBytes = uReadSectors << 9;
-
-        } else if (OPT_READ_BYTES == uSelectedOption) {
-          uReadBytes++;
-
-        }
-
-      } else if (uKeyPressed == CHAR_ENTER_BIG || uKeyPressed == CHAR_ENTER_SMALL || uKeyPressed == CHAR_COPY) {
-        if (OPT_DRIVE == uSelectedOption) {
+  // Main app loop
+  do {
+    g_u8KeyPressed = firm_read_key();
+    if (g_u8KeyPressed) {
+      if (FIRM_KEY_CURSOR_UP == g_u8KeyPressed && g_u8SelectedOption) {
+        g_u8SelectedOption--;
+      } else if (FIRM_KEY_CURSOR_DOWN == g_u8KeyPressed && g_u8SelectedOption < (OPTION_COUNT-1)) {
+        g_u8SelectedOption++;
+      } else if ('1' == g_u8KeyPressed) {
           fdc_toggleDrives();
-
-        } else if (OPT_MOTOR == uSelectedOption) {
-          fdc_toggleMotor();
-
-        } else if (OPT_TRACK == uSelectedOption) {
-          fdc_GoToTrack();
-
-        } else if (OPT_SECTI == uSelectedOption) {
-          fdc_GoToTrack();
+      } else if ('2' == g_u8KeyPressed) {
+        fdc_toggleMotor();
+      } else if ('q' == g_u8KeyPressed) {
+        g_u8PartialSecs -= 1;
+        g_u16PartialInts -= 300;
+      } else if ('w' == g_u8KeyPressed) {
+        g_u8PartialSecs += 1;
+        g_u16PartialInts += 300;
+      } else if ('a' == g_u8KeyPressed) {
+        g_fdc_u8TrackSelection--;
+      } else if ('s' == g_u8KeyPressed) {
+        g_fdc_u8TrackSelection++;
+      } else if ('z' == g_u8KeyPressed) {
+        g_fdc_u8SectorSelection--;
+      } else if ('x' == g_u8KeyPressed) {
+        g_fdc_u8SectorSelection++;
+      } else if (g_u8KeyPressed == FIRM_KEY_ENTER_BIG || g_u8KeyPressed == FIRM_KEY_ENTER_SMALL || g_u8KeyPressed == FIRM_KEY_COPY) {
+        if (OPT_TEST_SECID == g_u8SelectedOption) {
+          prepareHeadToTrack();
           g_bSectorIDNotFound = fdc_FindSector();
-
-        } else if (OPT_RPM == uSelectedOption) {
-          if (!g_sTime) {
+          fdc_TurnMotorOff();
+        } else if (OPT_TEST_RPM == g_u8SelectedOption) {
+          if (!g_u16Time) {
             startRPMs();
           } else {
             fdc_toggleMotor();
           }
-        
-        } else if ((OPT_READ_SECTORS == uSelectedOption) || (OPT_READ_BYTES == uSelectedOption)) {
-          myReadBytes();
-        } else if (OPT_TEST_TRACKS == uSelectedOption) {
+        } else if (OPT_TEST_TRACKS == g_u8SelectedOption) {
           testTracks();
         }
       }
 
       // Update measure rpm status based on motor status
       if (!g_fdc_u8MotorOn) {
-        printText("\x1F\x0A\x05        ");
-        g_sTime = 0;
-      } else if (g_sTime) {
-        g_sTime = 1;
-        uLoops = 0;
-        printText("\x1F\x0A\x05RUNNING!");
+        g_u16Time = 0;
+      } else if (g_u16Time) {
+        g_u16Time = 1;
+        g_u16Loops = 0;
       }
       printLabels();
     }
@@ -422,5 +343,3 @@ void main(void) {
     measureRPMs();
   } while(true);
 }
-
-
